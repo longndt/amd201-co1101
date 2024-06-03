3c056e87fda2d501152b88c26ade0c247a0e1f02
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-06-03 14:28:44 +0700

    tut1

---
+++ tut1/web1/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web1.Controllers
+{
+    //As default, Controller name is Subfolder name is Views
+    //Action (method) name is View name
+    /* For example:
+     * StudentController => need to create new subfolder named "Student" in "Views"
+     * Index() => need to create new view named "Index.cshtml" 
+     */
+
+    /* Controllers/StudentController.cs */
+    public class StudentController : Controller
+    {
+        //url1: localhost:PORT/Student/Index
+        //url2: localhost:PORT/Student
+        public IActionResult Index()
+        {
+            //path: Views/Student/Index.cshtml
+            return View();
+        }
+    }
+}
