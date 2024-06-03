9d253f5e531934eb6463ebb5e2873458e111f776
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-06-03 17:40:00 +0700

    tut1

--- tut1/web1/obj/Debug/net8.0/web1.AssemblyInfo.cs
+++ tut1/web1/obj/Debug/net8.0/web1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c056e87fda2d501152b88c26ade0c247a0e1f02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88454398071b94411d289f69c6bb1b7957b4981f")]
 [assembly: System.Reflection.AssemblyProductAttribute("web1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
