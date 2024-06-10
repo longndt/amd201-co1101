1c253a80dc4d3cf4ff06db49b6d0fa557a2d6caf
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-06-10 16:17:41 +0700

    tut2

--- tut2/web1/obj/Debug/net8.0/web1.AssemblyInfo.cs
+++ tut2/web1/obj/Debug/net8.0/web1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d253f5e531934eb6463ebb5e2873458e111f776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7d7920d29653e72c45b9d2a82331794047a2d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("web1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ tut2/web2/Models/Laptop.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web2.Models
+{
+    public class Laptop
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+
+        [MinLength(5, ErrorMessage = "Độ dài tối thiểu là 5")]
+        [MaxLength(30 , ErrorMessage = "Độ dài tối đa là 30")]
+        public string Model { get; set; }
+
+        [Range(1, 100, ErrorMessage = "Số lượng phải từ 1 đến 100")]
+        public int Quantity { get; set; }
+        
+        public decimal Price { get; set; }
+
+        public string Image { get; set; }
+
+        public string Color { get; set; }   
+    }
+}
